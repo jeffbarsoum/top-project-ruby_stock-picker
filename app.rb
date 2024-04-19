c976fdda39598987d867ea912476baa456640415
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-04-19 12:52:38 -0400

    Working wireframe

--- app.rb
+++ app.rb
@@ -9,15 +9,15 @@ def stock_picker stock_prices
     profit_vector = []
     day_vector = []
     stock_prices.each_with_index do |sell_price, sell_day|
-      profit_vector << buy_day < sell_day ? sell_price - buy_price : nil
-      day_vector << [buy_day, sell_day]
+      profit_vector.push buy_day < sell_day ? sell_price - buy_price : nil
+      day_vector.push [buy_day, sell_day]
     end
-    max_profit_day = profit_vector.each_with_index.max
-    max_profit_array <<  max_profit_day[0]
-    day_array << day_vector[max_profit_day[1]]
+    max_profit = profit_vector.compact.max
+    max_profit_array.push max_profit
+    day_array.push day_vector[profit_vector.find_index max_profit]
   end
-  max_profit = max_profit_array.each_with_index.max
-  buy_sell_day = day_array[max_profit[1]]
+  max_profit = max_profit_array.compact.max
+  buy_sell_day = day_array[max_profit_array.find_index max_profit]
 end
 
 stock_picker stock_prices
